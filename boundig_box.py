d0dd61c84630aeb003ef94e720991b83bbf85879
kiko77 <64518244+kiko77@users.noreply.github.com>
2021-05-23 20:51:56 +0800

    Update boundig_box.py

--- boundig_box.py
+++ boundig_box.py
@@ -6,12 +6,12 @@ def get_bounding_box(file_dir):
     new_dir = file_dir+'_box'
     os.mkdir(new_dir)
     for idx in range(33, 127):  # ASCII:33~126
-        word = cv2.imread(file_dir+'/'+str(idx)+'.jpg', cv2.IMREAD_GRAYSCALE)
+        word = cv2.imread(file_dir+'/'+str(idx)+'.png', cv2.IMREAD_GRAYSCALE)
         # boundary
         word_locs = np.argwhere(word==0)
         h1, w1 = word_locs.min(axis = 0)
         h2, w2 = word_locs.max(axis = 0)
         # bounding box
         word_bobox = word[h1:h2+1, w1:w2+1]
-        cv2.imwrite(new_dir+'/'+str(idx)+'.jpg', word_bobox)
+        cv2.imwrite(new_dir+'/'+str(idx)+'.png', word_bobox)
     return
