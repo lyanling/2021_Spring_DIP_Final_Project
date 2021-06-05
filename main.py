61c783e0093893926b0db1b39b3a3a9b239c9da4
lyanling <smile8www@gmail.com>
2021-06-05 10:40:31 +0800

    modify fcp

--- main.py
+++ main.py
@@ -7,17 +7,31 @@ from modules import find_cut_point as fcp
 import orientation as orien
 import bounding_box as bb
 from pathlib import Path
-
+import devide_parts as dp
 
 def save_cut_points(img, i, cut_points):
     cut_p_dir = 'cut_points/'
     Path(cut_p_dir).mkdir(parents=True, exist_ok=True)
-    cut_points_img = np.zeros_like(img)
-    cut_points_img = np.full(cut_points_img.shape, 255)
+    # cut_points_img = np.zeros_like(img)
+    cut_points_img = np.copy(img)
+    # cut_points_img = np.full(cut_points_img.shape, 255)
     cut_points_img_name = cut_p_dir+str(i)+'.png'
     for point in cut_points:
-        cut_points_img[tuple(point)] = 0
+        cut_points_img[tuple(point)] = 100
     cv2.imwrite(cut_points_img_name, cut_points_img.astype(np.uint8))
+
+def save_parts(img, i, parts):
+    part_dir = 'parts/'
+    Path(part_dir).mkdir(parents=True, exist_ok=True)
+    n = 0
+    for part in parts:
+        part_img_name = part_dir+str(i)+'_'+str(n)+'.png'
+        part_img = np.zeros_like(img)
+        part_img = np.full(part_img.shape, 255)
+        for point in part:
+            part_img[tuple(point)] = 0
+        cv2.imwrite(part_img_name, part_img.astype(np.uint8))
+        n += 1
 
 def save_theta(img, i):
     orientation_dir = 'orientation_imgs/'
@@ -25,7 +39,6 @@ def save_theta(img, i):
     file_name = orientation_dir + str(i) + '.png'
     img = (img - np.min(img)) / (np.max(img) - np.min(img)) * 255
     cv2.imwrite(file_name, img)
-
 
 # read in image
 frame_path = sys.argv[1]
@@ -37,8 +50,8 @@ start_letter = 'A'
 count, label = MP.objectCounting()
 prepros.extractFrames(MP.img_check, label, count, MP.img, ord(start_letter))
 
-start_idx = ord('A') # +1?
-end_idx = ord('A') # +1?
+start_idx = ord('A')+1  # 
+end_idx = ord('A')+1    # 
 file_dir = 'frames'
 file_dir = bb.get_bounding_box(file_dir, start_idx, end_idx)
 cut_points_list = []
@@ -48,7 +61,8 @@ for i in range(start_idx, end_idx+1):
     img = cv2.imread(file_dir+str(i)+'.png', cv2.IMREAD_GRAYSCALE)
     theta = orien.get_orientation(img)
     save_theta(theta, i)
-    cut_points = fcp.find_cut_point(img, theta, threshold=150)
-    cut_points_list.append(cut_points)
+    cut_points, parts = fcp.find_cut_point(img, theta)
     save_cut_points(img, i, cut_points)
+    # print(len(parts))
+    save_parts(img, i, parts)
 # print(cut_points_list)
